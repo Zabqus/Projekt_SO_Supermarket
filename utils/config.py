be9c958cf4ebc5d1b4c85ef3bd4afc6fefba6e3b
Zabqus <zabek.wiktor04@gmail.com>
2025-01-15 23:44:24 +0100

    dodanie konfiguracji

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,8 @@
-#ile kasjerow ogólnie
-#min kasjerów
-#klienci na kasjera
-#ilość osób na sklepie (?)
+class Config:
+    def __init__(self):
+        self.NUM_CASHIERS = 10          # całkowita liczba kas
+        self.MIN_ACTIVE_CASHIERS = 2    # min liczba aktywnych kas
+        self.CUSTOMERS_PER_CASHIER = 6  # liczba K z zadania - ilu klientów na jedną kasę
+        self.MAX_QUEUE_SIZE = 50        # max rozmiar kolejki
+
+#max osoby na skelep (?)
