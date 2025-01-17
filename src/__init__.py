657b16649b283b79ed425ccf10e3fe3d546687ca
Zabqus <zabek.wiktor04@gmail.com>
2025-01-17 22:40:54 +0100

    init src i utils

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .supermarket import Supermarket
+from .cashier import Cashier
+from .customer import Customer
+from .guard import SecurityGuard
+
+__all__ = ['Supermarket', 'Cashier', 'Customer', 'SecurityGuard']
---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+from .config import Config
+from .signal_handler import SignalHandler
+from .queue_manager import QueueManager
+
+__all__ = ['Config', 'SignalHandler', 'QueueManager']
