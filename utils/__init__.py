1aba66d808be860dda9eba7aa90603f98e4b6d72
Zabqus <zabek.wiktor04@gmail.com>
2025-01-29 15:42:51 +0100

    sygnalizacja

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
 from .config import Config
-from .signal_handler import SignalHandler
+from .signal_system import SignalHandler
 from .shared_memory_queue import QueueManager
 
 __all__ = ['Config', 'SignalHandler', 'QueueManager']
---
+++ utils/signal_system.py
@@ -0,0 +1,68 @@
+import os
+import signal
+import logging
+
+
+class SignalSystem:
+    def __init__(self):
+        self._fire_signal = False
+        self._child_pids = set()
+
+        '''Ustawienie obsługi sygnałów'''
+        signal.signal(signal.SIGUSR1, self._handle_fire)
+        signal.signal(signal.SIGCHLD, self._handle_child)
+
+    def _handle_fire(self, signum, frame):
+        if not self._fire_signal:
+            self._fire_signal = True
+            '''Przekazanie sygnału tylko do procesów potomnych'''
+            for pid in self._child_pids:
+                try:
+                    os.kill(pid, signal.SIGUSR1)
+                except ProcessLookupError:
+                    self._child_pids.discard(pid)
+
+    def _handle_child(self, signum, frame):
+        """Obsługa zakończenia procesu potomnego"""
+        try:
+            pid, status = os.waitpid(-1, os.WNOHANG)
+            if pid:
+                self._child_pids.discard(pid)
+        except ChildProcessError:
+            pass
+
+    def register_child(self, pid):
+        """Rejestracja nowego procesu potomnego"""
+        self._child_pids.add(pid)
+
+    def is_fire(self):
+        """Sprawdzenie czy jest pożar"""
+        return self._fire_signal
+
+    def set_fire(self):
+        """Ustawienie sygnału pożaru"""
+        if not self._fire_signal:
+            self._fire_signal = True
+
+            for pid in self._child_pids:
+                try:
+                    os.kill(pid, signal.SIGUSR1)
+                except ProcessLookupError:
+                    self._child_pids.discard(pid)
+
+    def clear_fire(self):
+        """Wyczyszczenie sygnału pożaru"""
+        self._fire_signal = False
+        for pid in self._child_pids:
+            try:
+                os.kill(pid, signal.SIGUSR2)
+            except ProcessLookupError:
+                self._child_pids.discard(pid)
+
+    def cleanup(self):
+        """Zakończenie wszystkich procesów potomnych"""
+        for pid in self._child_pids.copy():
+            try:
+                os.kill(pid, signal.SIGTERM)
+            except ProcessLookupError:
+                self._child_pids.discard(pid)
